8c881d3b4d927a80b702fc645a2ad1d177f20c7c
Ian Van Hoven <ianvanhoven@gmail.com>
2016-09-01 15:36:06 -0700

    revs

--- pkg/wxacct.py
+++ pkg/wxacct.py
@@ -1,30 +1,156 @@
 #! /usr/bin/python
 
+# +-------------------------------+
+# | account-related funcs...      |
+# | - [list|map] site account IDs |
+# | - list account item IDs       |
+# | - list account holdings       |
+# +-------------------------------+
+
+
+# +------------------------------------------------------------------------------------------+
+# | CAVEAT: no freaking clue how "site account ID" & "account item ID" differ :( yodlee #wtf |
+# +------------------------------------------------------------------------------------------+
+
+
+# slurp
 import json ;
+import os.path ;
 import requests ;
 import wxcore ;
 
-# +---------------------+
-# | account :: list IDs |
-# +---------------------+
 
-def list():
+# +--------------------------------------+
+# | account :: list (s)ite (a)ccount IDs |
+# +--------------------------------------+
 
-    la_auth = wxcore.auth() ;
-    la_data = { 'cobSessionToken' : la_auth[0] , 'userSessionToken' : la_auth[1] } ;
-    la_url  = wxcore.api_url + '/jsonsdk/SiteAccountManagement/getAllSiteAccounts' ;
-    la_post = requests.post( la_url , data=la_data ) ;
-    la_json = json.loads( la_post.text ) ;
+def list_acct_site():
 
-    # debug: print raw account listing
-    # print la_post.text.encode( 'utf-8' ) ;
+    as_auth = wxcore.auth() ;
+    as_data = { 'cobSessionToken' : as_auth[0] , 'userSessionToken' : as_auth[1] } ;
+    as_url  = wxcore.api_url + '/jsonsdk/SiteAccountManagement/getAllSiteAccounts' ;
+    as_post = requests.post( as_url , data=as_data ) ;
+    as_json = json.loads( as_post.text ) ;
 
-    la_dict = {} ;
+    # debug: spew raw account listing json
+    # print as_post.text.encode( 'utf-8' ) ;
 
-    for acct in la_json:
-        la_dict.update( { acct[ 'siteAccountId' ] : acct[ 'siteInfo' ][ 'defaultDisplayName' ] } ) ;
+    as_dict = {} ;
 
-    # payload
-    return la_dict ;
+    # return dict of {id,name} -- even though client may use mapped name instead (see below)
+    for acct in as_json:
+        as_dict.update( { acct[ 'siteAccountId' ] : acct[ 'siteInfo' ][ 'defaultDisplayName' ] } ) ;
+
+    # paydirt
+    return as_dict ;
+
+
+# +-----------------------------------------------------+
+# | account :: map (s)ite (a)ccount IDs to custom names |
+# +-----------------------------------------------------+
+
+def map_id_name( map_type ):
+
+    map_types = { \
+        'ai' : 'acct-item' , \
+        'as' : 'acct-site'
+        } ;
+
+    map_data = {} ;
+
+    map_file = wxcore.api_dir_data + 'wx.map.' + map_types[ map_type ] + '.txt' ;
+
+    if os.path.isfile( map_file ):
+        f = open( map_file , 'r' ) ;
+        map_line = f.readlines() ;
+        f.close() ;
+
+    else: print '[pkg/wxacct.py] ERROR: Cannot find/open id -> name mapping file' ; exit(1) ;
+
+    map_len = len( map_line ) ;
+
+    for i in range ( 0 , map_len ):
+        #map_line[i].rstrip() ;
+        map_item = map_line[i].rstrip().split( '#' ) ;
+        map_data.update( { map_item[0] : map_item[1] } ) ;
+
+    return map_data ;
+    
+ 
+# +--------------------------------------+
+# | account :: list (a)ccount (i)tem IDs |
+# +--------------------------------------+
+
+def list_acct_item():
+
+    # list of (s)ite (a)ccount IDs
+    as_dict = list_acct_site() ;
+
+    # request params for list of (a)ccount (i)tems
+    ai_auth = wxcore.auth() ;
+    ai_url  = wxcore.api_url + '/jsonsdk/DataService/getItemSummariesForSite' ;
+
+    ai_dict = {} ;
+
+    for id in as_dict:
+
+        ai_data = { \
+            'cobSessionToken'  : ai_auth[0] , \
+            'userSessionToken' : ai_auth[1] , \
+            'memSiteAccId'     : id
+        } ;
+
+        ai_post = requests.post( ai_url , data=ai_data ) ;
+        ai_json = json.loads( ai_post.text ) ;
+
+        # debug: print raw account item listing
+        # print id , '\n' , ai_post.text ; # .encode( 'utf-8' ) ;
+
+        for item in ai_json:
+
+            # +---------------------+
+            # | include...          |
+            # | - key: account ID   |
+            # | - val: account name |
+            # | - val: account type |
+            # +---------------------+
+
+            if 'itemDisplayName' in item:
+                ai_dict.update( { \
+                    item[ 'itemId' ] : [ item[ 'itemDisplayName' ] , \
+                    item[ 'contentServiceInfo' ][ 'containerInfo' ][ 'containerName' ] ] \
+                } ) ;
+
+    # paydirt
+    return ai_dict ;
+
+
+# +---------------------------------------------+
+# | account :: list (a)ccount (i)tem (h)oldings |
+# +---------------------------------------------+
+
+def list_acct_item_holding( aih_item ):
+
+    aih_auth = wxcore.auth() ;
+    aih_url  = wxcore.api_url + '/jsonsdk/DataService/getItemSummaryForItem1' ;
+
+    # see http://goo.gl/pvGHf1 for dex.extentLevels[*] settings
+    aih_data = {
+        'cobSessionToken'     : aih_auth[0] ,
+        'userSessionToken'    : aih_auth[1] ,
+        'itemId'              : aih_item ,
+        'dex.startLevel'      : 0 ,
+        'dex.endLevel'        : 0 ,
+        'dex.extentLevels[0]' : 0 ,
+        'dex.extentLevels[1]' : 2
+    } ;
+
+    # http post -> json response
+    aih_post = requests.post( aih_url , data=aih_data ) ;
+    aih_json = json.loads( aih_post.text ) ;
+
+    # paydirt
+    return aih_json ;
+
 
 # fin
--- pkg/wxcore.py
+++ pkg/wxcore.py
@@ -1,5 +1,10 @@
 #! /usr/bin/python
 
+
+# +-----------+
+# | saddle up |
+# +-----------+
+
 import json ;
 import os.path ;
 import requests ;
@@ -10,8 +15,11 @@ import time ;
 # | globals |
 # +---------+
 
-api_url = 'https://rest.developer.yodlee.com/services/srest/restserver/v1.0/' ;
+api_dir = '/space/home/ivh/tool/wealthx/' ;
+api_dir_data = api_dir + 'data/' ;
 api_hdr_json = { 'Content-Type' : 'application/json' , 'Accept' : 'application/json' } ;
+api_hdr_xml = { 'Content-Type' : 'application/xml' , 'Accept' : 'application/xml' } ;
+api_url = 'https://rest.developer.yodlee.com/services/srest/restserver/v1.0/' ;
 
 
 # +----------------------------------------------+
@@ -23,10 +31,12 @@ def auth():
     # check for existing valid token(s)...
     # - co-brand token expires every 100m
     # - user login token expires every 30m
-    # ...so 29m as expiry for both
+    # ...which is annoying/pointless...
+    # ...so 29m as co-expiry for simplicity
 
+    wx_auth_dir = api_dir + 'data/' ;
     wx_auth_token_new = 1 ;
-    wx_auth_token_txt = 'auth/wx.auth.token.txt' ;
+    wx_auth_token_txt = wx_auth_dir + 'wx.auth.token.txt' ;
     wx_time_now = int( time.time() ) ;
 
     # check presence/age of existing auth tokens
@@ -38,45 +48,45 @@ def auth():
 
         # use existing unexpired tokens
         wx_time_old = int( wx_auth_token_data[0] ) ;
-        if wx_time_now - wx_time_old <= 1740: # 29:00
+        if wx_time_now - wx_time_old <= 1740: # 29:00 (see above ^)
             cb_auth = wx_auth_token_data[1] ;
             ul_auth = wx_auth_token_data[2] ;
             wx_auth_token_new = 0 ;
 
-    else: print 'ERROR: Cannot find/open token auth file' ; exit(1) ;
+    else: print '[pkg/wxcore.py] ERROR: Cannot find/open token auth file' ; exit(1) ;
 
     # existing auth tokens missing or expired: create/store new tokens
     if wx_auth_token_new == 1:
 
         # co-brand
-        wx_auth_cobrand_txt = 'auth/wx.auth.cobrand.txt' ;
+        wx_auth_cobrand_txt = wx_auth_dir + 'wx.auth.cobrand.txt' ;
         if os.path.isfile( wx_auth_cobrand_txt ):
             f = open( wx_auth_cobrand_txt , 'r' ) ;
             wx_auth_cobrand_data = f.readlines() ;
             f.close() ;
-        else: print 'ERROR: Cannot find/open cobrand auth file' ; exit(1) ;
+        else: print '[pkg/wxcore.py] ERROR: Cannot find/open cobrand auth file' ; exit(1) ;
 
         cb_data = { 'cobrandLogin' : wx_auth_cobrand_data[0].rstrip() , 'cobrandPassword' : wx_auth_cobrand_data[1].rstrip() } ;
         cb_url  = api_url + 'authenticate/coblogin' ;
         cb_post = requests.post( cb_url , data=cb_data ) ;
-        if cb_post.status_code != 200: print 'ERROR: Non-200 REST API server response (Co-Brand Login), exiting' ; exit(1) ;
-        if 'sessionToken' not in cb_post.text: print 'ERROR: Co-Brand Login session token missing, exiting' ; exit(1) ;
+        if cb_post.status_code != 200: print '[pkg/wxcore.py] ERROR: Non-200 REST API server response (Co-Brand Login), exiting' ; exit(1) ;
+        if 'sessionToken' not in cb_post.text: print '[pkg/wxcore.py] ERROR: Co-Brand Login session token missing, exiting' ; exit(1) ;
         cb_dict = json.loads( cb_post.text ) ;
         cb_auth = cb_dict[ 'cobrandConversationCredentials' ][ 'sessionToken' ] ;
 
         # user login
-        wx_auth_user_txt = 'auth/wx.auth.user.txt' ;
+        wx_auth_user_txt = wx_auth_dir + 'wx.auth.user.txt' ;
         if os.path.isfile( wx_auth_user_txt ):
             f = open( wx_auth_user_txt , 'r' ) ;
             wx_auth_user_data = f.readlines() ;
             f.close() ;
-        else: print 'ERROR: Cannot find/open user auth file' ;
+        else: print '[pkg/wxcore.py] ERROR: Cannot find/open user auth file' ;
 
         ul_data = { 'cobSessionToken' : cb_auth , 'login' : wx_auth_user_data[0].rstrip() , 'password' : wx_auth_user_data[1].rstrip() } ;
         ul_url  = api_url + 'authenticate/login' ;
         ul_post = requests.post( ul_url , data=ul_data ) ;
-        if ul_post.status_code != 200: print 'ERROR: Non-200 REST API server response (User Login), exiting' ; exit(1) ;
-        if 'sessionToken' not in ul_post.text: print 'ERROR: User Login session token missing, exiting' ; exit(1) ;
+        if ul_post.status_code != 200: print '[pkg/wxcore.py] ERROR: Non-200 REST API server response (User Login), exiting' ; exit(1) ;
+        if 'sessionToken' not in ul_post.text: print '[pkg/wxcore.py] ERROR: User Login session token missing, exiting' ; exit(1) ;
         ul_dict = json.loads( ul_post.text ) ;
         ul_auth = ul_dict[ 'userContext' ][ 'conversationCredentials' ][ 'sessionToken' ] ;
 
@@ -85,7 +95,7 @@ def auth():
         f.write( '{:.0f}'.format( wx_time_now ) + '\n' + cb_auth + '\n' + ul_auth ) ;
         f.close() ;
 
-    # payload
+    # paydirt
     return [ cb_auth , ul_auth ] ;
 
 # fin
